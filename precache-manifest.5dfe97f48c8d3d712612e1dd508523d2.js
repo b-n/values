52d486d1e9f51e21e4e05928ceee2104bc833a43
Ben Naylor <nayben@gmail.com>
2020-03-25 20:22:57 +0100

    Updates

---
+++ precache-manifest.5dfe97f48c8d3d712612e1dd508523d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7696cc983bf74555c538f6efa4b76d6d",
+    "url": "/values/index.html"
+  },
+  {
+    "revision": "bd30bfa84cdab2b143ec",
+    "url": "/values/static/css/main.ff7a06d5.chunk.css"
+  },
+  {
+    "revision": "23875f71a7447eede79f",
+    "url": "/values/static/js/2.4f9d3977.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/values/static/js/2.4f9d3977.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd30bfa84cdab2b143ec",
+    "url": "/values/static/js/main.e70847d9.chunk.js"
+  },
+  {
+    "revision": "186cde24725c16fda882",
+    "url": "/values/static/js/runtime-main.aefb239c.js"
+  }
+]);
